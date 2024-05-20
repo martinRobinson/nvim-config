200b5b0c39012438de058b108053d47aed690028
Martin Robinson <martin.robinson@atu.ie>
2024-05-20 12:50:55 +0100

    New configutation for astronvim v4. Initial commit.

---
+++ lua/plugins/tabularize.lua
@@ -0,0 +1,4 @@
+return{
+	"godlygeek/tabular",
+	lazy = false,
+}
---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/zen-mode.nvim",
+  lazy = false,
+  opts = {
+		window = {
+			width = 120,
+		},
+		plugins = {
+			twilight = { enabled = true },
+		}
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
